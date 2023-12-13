35de797c7396352d5850870b7b575cb55b9acb28
H0llyW00dzZ <priv8@btz.pm>
2023-12-13 18:33:59 +0700

    Fix [Package] [shortid]
    - [+] fix(randshort.go): adjust buffer size to ensure enough characters after base64 encoding
    - [+] fix(randshort.go): ensure enough encoded characters to satisfy the requested length
    - [+] fix(randshort.go): trim to the requested length and remove any base64 padding

--- shortid/randshort.go
+++ shortid/randshort.go
@@ -7,11 +7,15 @@ import (
 
 // Generate creates a URL-friendly short ID.
 func Generate(length int) (string, error) {
-	// Adjust length to account for base64 encoding
-	encodedLength := length * 3 / 4
+	// Generate a buffer larger than needed to ensure we have enough characters
+	// after base64 encoding to satisfy the requested length.
+	bufferSize := length * 3 / 4
+	if length%3 != 0 {
+		bufferSize++ // Compensate for partial encoding groups
+	}
 
 	// Generate random bytes
-	randomBytes := make([]byte, encodedLength)
+	randomBytes := make([]byte, bufferSize)
 	if _, err := rand.Read(randomBytes); err != nil {
 		return "", err
 	}
@@ -19,8 +23,17 @@ func Generate(length int) (string, error) {
 	// URL-friendly encoding
 	encoded := base64.URLEncoding.EncodeToString(randomBytes)
 
-	// Trim padding
-	encoded = encoded[:length]
+	// Ensure we have enough encoded characters to satisfy the requested length
+	for len(encoded) < length {
+		extraBytes := make([]byte, 1)
+		if _, err := rand.Read(extraBytes); err != nil {
+			return "", err
+		}
+		encoded += base64.URLEncoding.EncodeToString(extraBytes)
+	}
 
+	// Trim to the requested length and remove any base64 padding
+	encoded = encoded[:length]
+	encoded = string([]rune(encoded)) // Convert to slice of runes to handle multi-byte characters
 	return encoded, nil
 }
