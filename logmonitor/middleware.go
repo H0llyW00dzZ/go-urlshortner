75b91668b675ae58c8a18ffdc51c0693dfe20603
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 01:41:39 +0700

    Chore [Package] [logmonitor] Context Logs
    - [+] chore(middleware.go): add context logs for different components

--- logmonitor/middleware.go
+++ logmonitor/middleware.go
@@ -9,6 +9,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Define context logs for different components.
+const (
+	URLmissmatchContextLog                  = "URL mismatch"
+	NoURLContextLog                         = "No URL found"
+	NoIDContextLog                          = "No ID found"
+	NoURLIDContextLog                       = "No URL and ID found"
+	NoURLIDDBContextLog                     = "No URL and ID found in DB"
+	URLupdateContextLog                     = "URL updated"
+	URLdeleteContextLog                     = "URL deleted"
+	ServerStartContextLog                   = "Server is starting and Listening on address"
+	ServerFailContextLog                    = "Server failed to start"
+	SignalContextLog                        = "Signal received"
+	ServerForcetoShutdownContextLog         = "Server is forced to shutdown:"
+	DataStoreFailContextLog                 = "Datastore failed to connect"
+	StartupFailedContextLog                 = "Startup failed"
+	StartupFailureContextLog                = "Startup failure"
+	FailedtoCloseDatastoreContextLog        = "failed to close datastore client:"
+	DatastoreFailedtoCheckHealthContextLog  = "datastore client failed health check:"
+	FailedToCreateDatastoreClientContextLog = "failed to create datastore client:"
+	FailedToIntializeLoggerContextLog       = "failed to initialize logger:"
+	DataStoreProjectIDEnvContextLog         = "DATASTORE_PROJECT_ID environment variable not set"
+)
+
 // Define emojis for different log levels.
 const (
 	ErrorEmoji           = "❌"
