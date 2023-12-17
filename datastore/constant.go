0866ecac65524b9ac3ee8dc6c27d8ce10b070811
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 04:17:29 +0700

    Chore & Fix [Package] [datastore] NoSQL Key Kind
    - [+] chore(constant.go): add DataStoreNameKey constant for the name of the Kind in Datastore for URL entities
    - [+] fix(nosql.go): use DataStoreNameKey constant instead of hardcoding the Kind name in Datastore operations

--- datastore/constant.go
+++ datastore/constant.go
@@ -5,4 +5,7 @@ package datastore
 // Easy Maintenance: If the error message changes, it only need to change it in one place.
 const (
 	DataStoreNosuchentity = "datastore: no such entity"
+	// DataStoreNameKey is the name of the Kind in Datastore for URL entities.
+	// Defining it here enables changing the Kind name in one place if needed.
+	DataStoreNameKey = "urlz"
 )
