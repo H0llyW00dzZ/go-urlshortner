1b2beeb8e8d824dca94c3e01ee4668ea1885aced
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 02:11:27 +0700

    Chore [Package] [logmonitor] Constant Header Response
    - [+] chore(header_response.go): update header response constant names for consistency and clarity

--- logmonitor/header_response.go
+++ logmonitor/header_response.go
@@ -2,6 +2,6 @@ package logmonitor
 
 // Define header response for different components.
 const (
-	HeaderResponseError          = "error"
-	HeaderResponseInternalServer = "internal server error"
+	HeaderResponseError               = "error"
+	HeaderResponseInternalServerError = "internal server error"
 )
