ed459ce43d4d57996cc35bcb751743cb99200996
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 02:40:59 +0700

    Chore & Feat Constant Header (#58)
    - [+] chore(header_constant.go): update header response constants
    - [+] feat(header_constant.go): add new header response constants

--- logmonitor/header_constant.go
+++ logmonitor/header_constant.go
@@ -2,9 +2,14 @@ package logmonitor
 
 // Define header response for different components.
 const (
-	HeaderResponseError               = "error"
-	HeaderResponseInternalServerError = "internal server error"
-	HeaderResponseshortened_url       = "shortened_url"
+	HeaderResponseError                     = "error"
+	HeaderResponseInternalServerError       = "internal server error"
+	HeaderResponseshortened_url             = "shortened_url"
+	HeaderResponseURlUpdated                = "URL updated successfully"
+	HeaderResponseInvalidRequestJSONBinding = "Invalid request - JSON binding error"
+	HeaderResponseInvalidURLFormat          = "Invalid URL format"
+	HeaderResponseURLDeleted                = "URL deleted successfully"
+	HeaderResponseIDandURLNotFound          = "ID and URL not found"
 )
 
 // Define header request for different components.
