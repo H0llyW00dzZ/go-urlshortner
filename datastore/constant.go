581bbdda37e32d78a9e2ce2a8c4f7d3ccf9c67b1
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 06:41:55 +0700

    Fix & Feat [Package] [Datastore] NoSQL Constant
    - [+] fix(constant.go): fix typo in constant name DataStoreNosuchentity
    - [+] feat(constant.go): add constants for failed operations in datastore
    - [+] fix(nosql.go): fix error message in SaveURL function
    - [+] fix(nosql.go): fix error message in UpdateURL function
    - [+] fix(nosql.go): fix error message in DeleteURL function
    - [+] fix(nosql.go): fix error message in CloseClient function

--- datastore/constant.go
+++ datastore/constant.go
@@ -4,7 +4,14 @@ package datastore
 // This is used to identify the component that is logging the message.
 // Easy Maintenance: If the error message changes, it only need to change it in one place.
 const (
-	DataStoreNosuchentity = "datastore: no such entity"
+	DataStoreNosuchentity         = "datastore: no such entity"
+	DataStoreFailedtoCreateClient = "Failed to create client"
+	DataStoreFailedtoSaveURL      = "Failed to save URL"
+	DataStoreFailedtoGetURL       = "Failed to get URL"
+	DataStoreFailedtoUpdateURL    = "Failed to update URL"
+	DataStoreFailedtoDeleteURL    = "Failed to delete URL"
+	DataStoreFailedToCloseClient  = "Failed to close client"
+
 	// DataStoreNameKey is the name of the Kind in Datastore for URL entities.
 	// Defining it here enables changing the Kind name in one place if needed.
 	DataStoreNameKey = "urlz"
