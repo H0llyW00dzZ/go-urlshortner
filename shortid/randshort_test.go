a171db4b812491707fda5d3d36224557fb8ea02e
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 15:05:06 +0700

    Fix [Package] [ShortID] length (#71)
    * Fix [Package] [ShortID] length
    
    - [+] fix(randshort.go): add input validation for length parameter in Generate function
    
    * Test [Package] [ShortID]
    
    - [+] test(randshort_test.go): add test for expected length of generated short ID
    - [+] test(randshort_test.go): add test for uniqueness of generated short IDs
    - [+] test(randshort_test.go): add test for boundary conditions of Generate function

--- shortid/randshort_test.go
+++ shortid/randshort_test.go
@@ -47,3 +47,46 @@ func TestGenerateLengthVariance(t *testing.T) {
 		}
 	}
 }
+
+// TestGenerate_ExpectedLength tests that the Generate function returns a string of the expected length.
+func TestGenerate_ExpectedLength(t *testing.T) {
+	lengths := []int{6, 8, 10} // More realistic lengths for a short ID
+	for _, length := range lengths {
+		id, err := Generate(length)
+		if err != nil {
+			t.Fatalf("Generate(%d) returned an unexpected error: %v", length, err)
+		}
+		if got := len(id); got != length {
+			t.Errorf("Generate(%d) returned a string of length %d, want %d", length, got, length)
+		}
+	}
+}
+
+// TestGenerate_Uniqueness checks if the Generate function returns unique values on subsequent calls.
+func TestGenerate_Uniqueness(t *testing.T) {
+	length := 5       // A more realistic length for a short ID
+	iterations := 100 // Reduce the number of iterations
+	ids := make(map[string]bool)
+
+	for i := 0; i < iterations; i++ {
+		id, err := Generate(length)
+		if err != nil {
+			t.Fatalf("Generate returned an unexpected error: %v", err)
+		}
+		if ids[id] {
+			t.Fatalf("Generate returned a non-unique id: %v", id)
+		}
+		ids[id] = true
+	}
+}
+
+// TestGenerate_BoundaryConditions tests that the Generate function returns an error for invalid lengths.
+func TestGenerate_BoundaryConditions(t *testing.T) {
+	testCases := []int{0, -1}
+	for _, length := range testCases {
+		_, err := Generate(length)
+		if err == nil {
+			t.Errorf("Generate(%d) should return an error", length)
+		}
+	}
+}
