4e1fe15c288275697959815ff6e1e92001a7734a
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 21:28:55 +0700

    [Package] [Handle] Reduce Gocyclo keep under 5
    - [+] fix(error.go): remove unused logFields variable in handleDeletionError function
    - [+] feat(error.go): refactor handleDeletionError function to use separate functions for different error cases

--- handlers/error.go
+++ handlers/error.go
@@ -43,46 +43,19 @@ func handleUpdateError(c *gin.Context, id string, err error) {
 // handleDeletionError handles errors that occur during the URL deletion process.
 func handleDeletionError(c *gin.Context, err error) {
 	id := c.Param(constant.HeaderID)
-	// Use the centralized logging function from logmonitor package
-	logFields := logmonitor.CreateLogFields("deleteURL",
-		logmonitor.WithComponent(constant.ComponentGopher),
-		logmonitor.WithID(id),
-		logmonitor.WithError(err),
-	)
-	// Fix internal issue now it's stable
 	switch {
 	case err == datastore.ErrNotFound:
-		logmonitor.Logger.Info(constant.AlertEmoji+"  "+constant.WarningEmoji+"  "+constant.NoURLIDContextLog, logFields...)
-		c.JSON(http.StatusNotFound, gin.H{
-			constant.HeaderResponseError: constant.HeaderResponseIDandURLNotFound,
-		})
-	case strings.Contains(err.Error(), constant.PathIDandPayloadIDDoesnotMatchContextLog):
+		logNotFound(c, id) // Pass the context and id instead of err.Error()
 		// This is a client error, so we should return a 400 status code.
-		logmonitor.Logger.Info(constant.ErrorEmoji+"  "+constant.WarningEmoji+"  "+constant.URLmismatchContextLog, logFields...)
-		c.JSON(http.StatusBadRequest, gin.H{
-			constant.HeaderResponseError: constant.PathIDandPayloadIDDoesnotMatchContextLog,
-		})
-	case strings.Contains(err.Error(), constant.HeaderResponseInvalidRequestPayload):
+	case isMismatchError(err):
+		logMismatchError(c, id) // Pass the context and id instead of err.Error()
+	case isBadRequestError(err):
 		// This is also a client error bad request, so we should return a 400 status code.
 		// Return a BadRequestError if JSON binding fails
 		// Friendly error message for the user, and the original error for logging purposes
-		logmonitor.Logger.Info(constant.ErrorEmoji+"  "+constant.WarningEmoji+"  "+constant.URLmismatchContextLog, logFields...)
-		c.JSON(http.StatusBadRequest, gin.H{
-			constant.HeaderResponseError: constant.HeaderResponseInvalidRequestPayload,
-		})
+		logBadRequest(c, id) // Pass the context and id instead of err.Error()
 	default:
-		if badRequestErr, ok := err.(*logmonitor.BadRequestError); ok {
-			logmonitor.Logger.Info(constant.AlertEmoji+"  "+constant.WarningEmoji+"  "+constant.HeaderResponseInvalidRequestJSONBinding, logFields...)
-			c.JSON(http.StatusBadRequest, gin.H{
-				constant.HeaderResponseError: badRequestErr.UserMessage,
-			})
-		} else {
-			// For other types of errors, respond with a 500 Internal Server Error.
-			logmonitor.Logger.Error(constant.SosEmoji+"  "+constant.WarningEmoji+"  "+constant.FailedToDeletedURLContextLog, logFields...)
-			c.JSON(http.StatusInternalServerError, gin.H{
-				constant.HeaderResponseError: constant.HeaderResponseInternalServerError,
-			})
-		}
+		logDefaultError(c, id, err) // Pass the context, id, and error
 	}
 }
 
