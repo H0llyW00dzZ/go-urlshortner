eb3bee177c47779a0ccf4c8f21e370567aba1cc3
H0llyW00dzZ <priv8@btz.pm>
2023-12-18 08:58:37 +0700

    Chore [Package] [datastore] Constant
    - [+] chore(constant.go): add URL info messages for attempting and failing to update URL in Datastore

--- datastore/constant.go
+++ datastore/constant.go
@@ -15,4 +15,8 @@ const (
 	// DataStoreNameKey is the name of the Kind in Datastore for URL entities.
 	// Defining it here enables changing the Kind name in one place if needed.
 	DataStoreNameKey = "urlz"
+
+	// URL Info Messages
+	InfoAttemptingToUpdateURLInDatastore = "Attempting to update URL in Datastore"
+	InfoFailedToUpdateURLInDatastore     = "Failed to update URL in Datastore"
 )
