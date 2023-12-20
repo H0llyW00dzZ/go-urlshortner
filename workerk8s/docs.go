230f3c66b9d3f070635d7f48f13d9202fd0dea2c
H0llyW00dzZ <priv8@btz.pm>
2023-12-21 03:43:27 +0700

    [Package] [WorkerK8S] Feat Context & Update Docs
    - [+] feat(workerk8s): add error message for retrieving pods
    - [+] feat(workerk8s): add cancellation message for worker process
    - [+] fix(workerk8s): update Worker function to use context for cancellation or timeouts
    - [+] fix(workerk8s): update RunWorkers function to pass context to Worker function
    - [+] docs(workerk8s): update usage example and add TODOs for future improvements

--- workerk8s/docs.go
+++ workerk8s/docs.go
@@ -10,16 +10,17 @@
 //
 //   - NewKubernetesClient: Establishes a new connection to the Kubernetes API server using
 //     in-cluster configuration and returns a Kubernetes clientset.
-//   - Worker: A function that performs operations using the Kubernetes client. It sends
-//     the results of its operation to a channel for collection and further processing.
+//   - Worker: A function that performs health checks on pods within a given namespace and
+//     sends the results to a channel for collection and further processing. It respects
+//     the context passed to it for cancellation or timeouts.
 //   - RunWorkers: Starts a specified number of worker goroutines that call the Worker function
-//     and collects the results from all workers into a slice of strings.
+//     with a given namespace and collects the results from all workers into a slice of strings.
 //
 // # Usage
 //
 // To use this package, first create a Kubernetes client by calling NewKubernetesClient.
 // Then, use the client to run worker goroutines with RunWorkers, specifying the number
-// of workers you want to start.
+// of workers and the namespace you want to target.
 //
 // # Example
 //
@@ -27,7 +28,8 @@
 //	if err != nil {
 //	    // Handle error
 //	}
-//	results := workerk8s.RunWorkers(clientset, 5)
+//	namespace := "default" // Replace with your namespace
+//	results := workerk8s.RunWorkers(clientset, 5, namespace)
 //	for _, result := range results {
 //	    fmt.Println(result)
 //	}
@@ -35,8 +37,10 @@
 // # TODO
 //
 //   - Implement error handling and retry logic within the Worker function to handle transient errors.
-//   - Provide a context with a timeout or cancellation to control the lifetime of worker operations.
 //   - Enhance the Worker function to perform a more specific task or to be more configurable.
 //   - Consider adding a function to clean up resources or to gracefully shut down the workers.
 //   - Expand the package to support other Kubernetes resources and operations.
+//   - Add context parameter to RunWorkers to control the lifetime of worker operations and allow for shutdown signals.
+//
+// Copyright (c) 2023 by H0llyW00dzZ
 package workerk8s
--- workerk8s/worker.go
+++ workerk8s/worker.go
@@ -9,31 +9,59 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
-// / Worker performs health checks on all pods in the given namespace and sends the results to a channel.
-func Worker(clientset *kubernetes.Clientset, namespace string, results chan<- string) {
-	// Retrieve a list of all pods in the specified namespace.
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), v1.ListOptions{})
+// Worker starts a worker process that retrieves all pods in a given namespace,
+// performs health checks on them, and sends the results to a channel.
+func Worker(ctx context.Context, clientset *kubernetes.Clientset, namespace string, results chan<- string) {
+	// Retrieve a list of pods from the namespace.
+	pods, err := getPods(ctx, clientset, namespace)
 	if err != nil {
-		results <- fmt.Sprintf("Error: %v", err)
+		// If there's an error retrieving pods, send an error message on the results channel.
+		results <- fmt.Sprintf("Error retrieving pods: %v", err)
 		return
 	}
 
-	// Perform health checks on each pod and send the results over the channel.
-	for _, pod := range pods.Items {
-		healthStatus := NotHealthyStatus
-		if isPodHealthy(&pod) {
-			healthStatus = HealthyStatus
+	// Process each pod to determine its health status and send the results on the channel.
+	processPods(ctx, pods, results)
+}
+
+// getPods fetches the list of all pods within a specific namespace.
+func getPods(ctx context.Context, clientset *kubernetes.Clientset, namespace string) ([]corev1.Pod, error) {
+	// List all pods in the namespace using the provided context.
+	podList, err := clientset.CoreV1().Pods(namespace).List(ctx, v1.ListOptions{})
+	if err != nil {
+		// Return an error if the pod list cannot be retrieved.
+		return nil, err
+	}
+	return podList.Items, nil
+}
+
+// processPods iterates over a slice of pods, performs a health check on each,
+// and sends a formatted status string to the results channel.
+func processPods(ctx context.Context, pods []corev1.Pod, results chan<- string) {
+	for _, pod := range pods {
+		select {
+		case <-ctx.Done():
+			// If the context is cancelled, send a cancellation message and exit the function.
+			results <- fmt.Sprintf(WorkerCancelled, ctx.Err())
+			return
+		default:
+			// Determine the health status of the pod and send the result.
+			healthStatus := NotHealthyStatus
+			if isPodHealthy(&pod) {
+				healthStatus = HealthyStatus
+			}
+			results <- fmt.Sprintf(PodAndStatusAndHealth, pod.Name, pod.Status.Phase, healthStatus)
 		}
-		results <- fmt.Sprintf(PodAndStatusAndHealth, pod.Name, pod.Status.Phase, healthStatus)
 	}
 }
 
-// isPodHealthy determines if a pod is considered "healthy" based on its phase and container statuses.
+// isPodHealthy checks if a given pod is in a running phase and all of its containers are ready.
 func isPodHealthy(pod *corev1.Pod) bool {
-	// A pod is considered healthy if it's running and all of its containers are ready.
+	// Check if the pod is in the running phase.
 	if pod.Status.Phase != corev1.PodRunning {
 		return false
 	}
+	// Check if all containers within the pod are ready.
 	for _, containerStatus := range pod.Status.ContainerStatuses {
 		if !containerStatus.Ready {
 			return false
