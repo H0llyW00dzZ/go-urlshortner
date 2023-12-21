d530484d1293645bf79f3e8e590df357311bf235
H0llyW00dzZ <priv8@btz.pm>
2023-12-21 08:38:36 +0700

    [Package] [WorkerK8S] Update Docs & Implement Zap Logger
    - [+] feat(workerk8s): add structured logging to Worker function
    - [+] feat(workerk8s): add structured logging to RunWorkers function
    - [+] fix(workerk8s): fix log messages in getPods function
    - [+] fix(workerk8s): fix log messages in processPods function
    - [+] chore(workerk8s): update TODO list in docs.go
    - [+] chore(workerk8s): update enhancements section in docs.go

--- workerk8s/docs.go
+++ workerk8s/docs.go
@@ -12,10 +12,11 @@
 //     in-cluster configuration and returns a Kubernetes clientset.
 //   - Worker: A function that performs health checks on pods within a given namespace and
 //     sends the results to a channel for collection and further processing. It respects
-//     the context passed to it for cancellation or timeouts.
+//     the context passed to it for cancellation or timeouts, and it utilizes a structured
+//     logger for enhanced logging with contextual information such as the namespace and task.
 //   - RunWorkers: Starts a specified number of worker goroutines that call the Worker function
-//     with a given namespace. It returns a channel for results and a function to initiate
-//     a graceful shutdown of the workers.
+//     with a given namespace and a structured logger. It returns a channel for results and a
+//     function to initiate a graceful shutdown of the workers.
 //
 // # Usage
 //
@@ -41,11 +42,19 @@
 //	    fmt.Println(result)
 //	}
 //
+// # Enhancements
+//
+//   - The Worker function now includes structured logging, which improves traceability and
+//     debugging by providing contextual information in the log entries.
+//   - The logging within the Worker function is now customizable, allowing different workers
+//     to log with their specific contextual information such as worker index and namespace.
+//
 // # TODO
 //
 //   - Implement error handling and retry logic within the Worker function to handle transient errors.
 //   - Enhance the Worker function to perform a more specific task or to be more configurable.
 //   - Expand the package to support other Kubernetes resources and operations.
+//   - Introduce metrics collection for monitoring the health and performance of the workers.
 //
 // Copyright (c) 2023 by H0llyW00dzZ
 package workerk8s
